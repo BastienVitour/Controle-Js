a3d1f821ef76fddbaf350d384eed0298af7faab9
BastienVitour <113524545+BastienVitour@users.noreply.github.com>
2022-11-25 13:02:15 +0100

    bonus 1

--- banque.js
+++ banque.js
@@ -9,54 +9,59 @@ account.addEventListener('input', background)
 let valid = true;
 let error = 'none';
 
+let accountNum = 0;
+let sumVal = sum.value;
+
+let index = 0
+
 // Ce qui se passe quand on envoie le formulaire
 function getInfos() {
 
     error = 'none';
     valid = true;
+    account.style.backgroundColor = '#FFFFFF'
 
     //On récupère le nom du compte
-    let accountNum = account.value
+    accountNum = account.value
+
+    //console.log(accountNum)
 
     //On vérifie la validité du compte
-    if (accountNum.length === 16) {
-        if (valid) {
-            account.value = "";
-            console.log(accountNum);
-        }
-    }
-    else {
-        error = 'compte'
-        valid = false;
-    }
-
-    //On récupère la somme sélectionnée
-    let sumVal = sum.value;
-
-    //On vérifie la validité de la somme entrée
-    if (isNaN(sumVal)) {
-        valid = false;
-        error = 'somme'
-    }
-    else {
-        if (sumVal <= 0) {
-            valid = false;
-            error = 'somme'
+    if (!isNaN(accountNum)) {
+        if (accountNum.length === 16) {
+            if (valid) {
+                account.value = "";
+                console.log(accountNum);
+            }
         }
         else {
-            if (valid) {
-                sum.value = "";
-                console.log(sumVal);
-            }
-            
+            error = 'compte'
+            valid = false;
         }
     }
+    else {
+        //console.log(Number.isInteger(accountNum))
+        error = 'compte';
+        valid = false;
+    }
+    
+
+    //On récupère la somme sélectionnée
+    sumVal = sum.value;
+
+    //On vérifie la validité de la somme entrée
+    checkNumber(sumVal)
 
     //Envoi de l'alerte de confirmation
     if (valid) {
         alert('La somme de '+sumVal+' a bien été déposée')
+        
+        console.log(index)
+        addTransaction(index);
+        index++
     }
     else {
+        //Envoi du message d'erreur
         alert('Erreur sur le champ '+error)
     }
         
@@ -71,4 +76,55 @@ function background () {
     else {
         account.style.backgroundColor = "#FFFFFF";
     }
+}
+
+function checkNumber(number) {
+    if (isNaN(number)) {
+        valid = false;
+        error = 'somme'
+    }
+    else {
+        if (number <= 0) {
+            valid = false;
+            error = 'somme'
+        }
+        else {
+            if (valid) {
+                sum.value = "";
+                console.log(number);
+            }
+            
+        }
+    }
+}
+
+let transactionList = document.querySelector('#transactions');
+let element = document.getElementsByClassName('transaction');
+let accounts = document.getElementsByClassName('account');
+let sums = document.getElementsByClassName('sum')
+
+function addTransaction(index) {
+
+    const addDiv = document.createElement("div");
+
+    addDiv.classList.add('transaction')
+
+    transactionList.appendChild(addDiv);
+
+    const addAccount = document.createElement("span")
+
+    addAccount.classList.add('account')
+
+    element[index].appendChild(addAccount)
+
+    const addSum = document.createElement("span")
+
+    addSum.classList.add('sum')
+
+    element[index].appendChild(addSum)
+
+    accounts[index].textContent = 'Compte '+accountNum+' ';
+
+    sums[index].textContent = 'a déposé '+sumVal;
+
 }
